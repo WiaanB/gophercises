6c60c5caef650c61cb3e724ab09e0a6a710f847c
WiaanB <whoppingwaki@gmail.com>
2022-09-02 19:53:53 +0200

    added yaml handler

--- urlshort/handler.go
+++ urlshort/handler.go
@@ -2,7 +2,14 @@ package urlshort
 
 import (
 	"net/http"
+
+	"gopkg.in/yaml.v2"
 )
+
+type pathURL struct {
+	Path string `yaml:"path"`
+	URL  string `yaml:"url"`
+}
 
 func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -13,4 +20,17 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 		}
 		fallback.ServeHTTP(w, r)
 	}
+}
+
+func YAMLHandler(yamlArr []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	var pathUrls []pathURL
+	err := yaml.Unmarshal(yamlArr, &pathUrls)
+	if err != nil {
+		return nil, err
+	}
+	pathsToUrls := make(map[string]string)
+	for _, pu := range pathUrls {
+		pathsToUrls[pu.Path] = pu.URL
+	}
+	return MapHandler(pathsToUrls, fallback), nil
 }
