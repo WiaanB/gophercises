05db84d36b56aecedf9e1a1eac4192a2b2077a99
wiaan <wiaan.botha@spandigital.com>
2022-09-17 19:56:09 +0200

    find and print all the a tags in the html

--- parser/parse.go
+++ parser/parse.go
@@ -1,6 +1,11 @@
 package link
 
-import "io"
+import (
+	"fmt"
+	"io"
+
+	"golang.org/x/net/html"
+)
 
 // Link represents a hypertext link in html
 type Link struct {
@@ -10,5 +15,24 @@ type Link struct {
 
 // parse will take in a HTML doc, and return a slice of links parsed from it
 func Parse(r io.Reader) ([]Link, error) {
+	doc, err := html.Parse(r)
+	if err != nil {
+		return nil, err
+	}
+	links := linkNodes(doc)
+	for _, l := range links {
+		fmt.Println(l)
+	}
 	return nil, nil
+}
+
+func linkNodes(n *html.Node) []*html.Node {
+	if n.Type == html.ElementNode && n.Data == "a" {
+		return []*html.Node{n}
+	}
+	var ret []*html.Node
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		ret = append(ret, linkNodes(c)...)
+	}
+	return ret
 }
