6453674324e70c675505abf1bea29869ffdd9b1d
WiaanB <whoppingwaki@gmail.com>
2022-09-21 20:26:13 +0200

    renamed a single (1) variable

--- sitemap/main.go
+++ sitemap/main.go
@@ -33,15 +33,15 @@ func main() {
 
 func hrefs(r io.Reader, base string) []string {
 	links, _ := link.Parse(r)
-	var hrefs []string
+	var ret []string
 	for _, l := range links {
 		switch {
 		case strings.HasPrefix(l.Href, "/"):
-			hrefs = append(hrefs, base+l.Href)
+			ret = append(ret, base+l.Href)
 		case strings.HasPrefix(l.Href, "http"):
-			hrefs = append(hrefs, l.Href)
+			ret = append(ret, l.Href)
 		}
 	}
 
-	return hrefs
+	return ret
 }
