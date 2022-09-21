6ce7bde51f2fe47417e9991d76ffb6a8a00a877f
WiaanB <whoppingwaki@gmail.com>
2022-09-21 20:28:19 +0200

    removed the get functionality into its own function too

--- sitemap/main.go
+++ sitemap/main.go
@@ -14,17 +14,7 @@ func main() {
 	urlFlag := flag.String("url", "https://gophercises.com", "url that you want to build a sitemap for")
 	flag.Parse()
 
-	resp, err := http.Get(*urlFlag)
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
-
-	reqUrl := resp.Request.URL
-	baseUrl := &url.URL{Scheme: reqUrl.Scheme, Host: reqUrl.Host}
-	base := baseUrl.String()
-
-	pages := hrefs(resp.Body, base)
+	pages := get(*urlFlag)
 
 	for _, page := range pages {
 		fmt.Println(page)
@@ -44,4 +34,20 @@ func hrefs(r io.Reader, base string) []string {
 	}
 
 	return ret
+}
+
+func get(urlStr string) []string {
+	resp, err := http.Get(urlStr)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	reqUrl := resp.Request.URL
+	baseUrl := &url.URL{Scheme: reqUrl.Scheme, Host: reqUrl.Host}
+	base := baseUrl.String()
+
+	pages := hrefs(resp.Body, base)
+
+	return pages
 }
