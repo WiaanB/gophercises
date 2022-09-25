a62d74d256001d8f394f16f44fcc5a9e994e5294
WiaanB <whoppingwaki@gmail.com>
2022-09-25 19:14:01 +0200

    added bfs to the sitemap builder

--- sitemap/main.go
+++ sitemap/main.go
@@ -12,9 +12,10 @@ import (
 
 func main() {
 	urlFlag := flag.String("url", "https://gophercises.com", "url that you want to build a sitemap for")
+	maxDepth := flag.Int("depth", 10, "how many levels deep links should traverse")
 	flag.Parse()
 
-	pages := get(*urlFlag)
+	pages := bfs(*urlFlag, *maxDepth)
 
 	for _, page := range pages {
 		fmt.Println(page)
@@ -64,4 +65,32 @@ func withPrefix(pfx string) func(string) bool {
 	return func(link string) bool {
 		return strings.HasPrefix(link, pfx)
 	}
+}
+
+func bfs(urlStr string, maxDepth int) []string {
+	seen := make(map[string]struct{})
+	var q map[string]struct{}
+	nq := map[string]struct{}{
+		urlStr: struct{}{},
+	}
+	for i := 0; i <= maxDepth; i++ {
+		q, nq = nq, make(map[string]struct{})
+		if len(q) == 0 {
+			break
+		}
+		for url, _ := range q {
+			if _, ok := seen[url]; ok {
+				continue
+			}
+			seen[url] = struct{}{}
+			for _, link := range get(url) {
+				nq[link] = struct{}{}
+			}
+		}
+	}
+	ret := make([]string, 0, len(seen))
+	for url, _ := range seen {
+		ret = append(ret, url)
+	}
+	return ret
 }
