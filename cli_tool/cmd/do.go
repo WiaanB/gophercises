5e4690d6d8324a3cc4404366c217fe2ad7e877f0
wiaan <whoppingwaki@gmail.com>
2022-10-07 10:43:32 +0200

    Completed the functionality for marking tasks as done

--- cli_tool/cmd/do.go
+++ cli_tool/cmd/do.go
@@ -2,7 +2,9 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 	"strconv"
+	"workspace/cli_tool/db"
 
 	"github.com/spf13/cobra"
 )
@@ -20,7 +22,27 @@ var doCmd = &cobra.Command{
 				ids = append(ids, id)
 			}
 		}
-		fmt.Println(ids)
+		tasks, err := db.AllTasks()
+		if err != nil {
+			if err != nil {
+				fmt.Println("Something went wrong: ", err.Error())
+				os.Exit(1)
+			}
+		}
+
+		for _, id := range ids {
+			if id <= 0 || id > len(tasks) {
+				fmt.Println("Invaled task number:", id)
+				continue
+			}
+			task := tasks[id-1]
+			err := db.DeleteTask(task.Key)
+			if err != nil {
+				fmt.Printf("Failed to mark \"%d\" as complete. Error: %s\n", id, err)
+			} else {
+				fmt.Printf("Marked \"%d\" as complete\n", id)
+			}
+		}
 	},
 }
 
