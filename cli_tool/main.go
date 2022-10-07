ae340eb01cc3fc5cfeff48a19669b3b71f639e6e
wiaan <whoppingwaki@gmail.com>
2022-10-07 09:16:19 +0200

    setup for db

--- cli_tool/main.go
+++ cli_tool/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "workspace/cli_tool/cmd"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"workspace/cli_tool/cmd"
+	"workspace/cli_tool/db"
+
+	"github.com/mitchellh/go-homedir"
+)
 
 func main() {
-	cmd.RootCmd.Execute()
+	home, _ := homedir.Dir()
+	dbPath := filepath.Join(home, "tasks.db")
+	must(db.Init(dbPath))
+	must(cmd.RootCmd.Execute())
+}
+
+func must(err error) {
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
 }
