2dbce3d29de48d98a37b3ad8e26deba3d65f3963
wiaan <whoppingwaki@gmail.com>
2022-10-10 10:47:28 +0200

    created the mai nfunction and added a test, to ensure the normalizer function works as expected

---
+++ number_normalizer/main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"regexp"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "admin"
+	password = "client_pass"
+	dbname   = "gophercises"
+)
+
+func main() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s sslmode=disable", host, port, user, password)
+
+	db, err := sql.Open("postgres", fmt.Sprintf("%s dbname=%s", psqlInfo, "postgres"))
+	must(err)
+	err = resetDB(db, dbname)
+	must(err)
+	db.Close()
+
+	psqlInfo = fmt.Sprintf("%s dbname=%s", psqlInfo, dbname)
+	db, err = sql.Open("postgres", psqlInfo)
+	must(err)
+	defer db.Close()
+
+	must(createNumberTable(db))
+	_, err = insertPhone(db, "1234567890")
+	must(err)
+	_, err = insertPhone(db, "123 456 7891")
+	must(err)
+	_, err = insertPhone(db, "(123) 456 7892")
+	must(err)
+	_, err = insertPhone(db, "(123) 456-7893")
+	must(err)
+	_, err = insertPhone(db, "123-456-7894")
+	must(err)
+	_, err = insertPhone(db, "123-456-7890")
+	must(err)
+	_, err = insertPhone(db, "1234567892")
+	must(err)
+	_, err = insertPhone(db, "(123)456-7892")
+	must(err)
+
+}
+
+func insertPhone(db *sql.DB, phone string) (int, error) {
+	statement := `INSERT INTO phone_numbers(value) VALUES($1) RETURNING id`
+	var id int
+	err := db.QueryRow(statement, phone).Scan(&id)
+	if err != nil {
+		return -1, err
+	}
+	return id, nil
+}
+
+func createNumberTable(db *sql.DB) error {
+	statement := `
+		CREATE TABLE IF NOT EXISTS phone_numbers (
+			id SERIAL,
+			value VARCHAR(255)
+		)`
+	_, err := db.Exec(statement)
+	return err
+}
+
+func resetDB(db *sql.DB, name string) error {
+	_, err := db.Exec("DROP DATABASE IF EXISTS " + name)
+	must(err)
+	return createDB(db, name)
+}
+
+func createDB(db *sql.DB, name string) error {
+	_, err := db.Exec("CREATE DATABASE " + name)
+	must(err)
+	return nil
+}
+
+func normalize(phone string) string {
+	re := regexp.MustCompile(`\D`)
+	return re.ReplaceAllString(phone, "")
+}
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
