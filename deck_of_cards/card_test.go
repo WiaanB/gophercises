530b91f51518356fc73deb0b8b8c0701ec07029d
wiaan <whoppingwaki@gmail.com>
2022-10-17 09:46:53 +0200

    added a deck function and a test for it

--- deck_of_cards/card_test.go
+++ deck_of_cards/card_test.go
@@ -67,3 +67,10 @@ func TestFilter(t *testing.T) {
 		}
 	}
 }
+
+func TestDeck(t *testing.T) {
+	cards := New(Deck(3))
+	if len(cards) != 13*4*3 {
+		t.Errorf("expected %d cards, recevied %d cards", 13*4*3, len(cards))
+	}
+}
