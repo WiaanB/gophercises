a2b551785b873524478e466c4770b0c3219e48e7
WiaanB <whoppingwaki@gmail.com>
2022-11-08 20:59:30 +0200

    file renamer

---
+++ file_renamer/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
+)
+
+func main() {
+	fileName := "birthday_001.txt" // => Birthday - 1 of 4.txt
+	newName, err := match(fileName, 4)
+	if err != nil {
+		fmt.Println("no match")
+		os.Exit(1)
+	}
+	fmt.Println(newName)
+}
+
+// match returns the new file name, or an error if the file name
+// didn't match our pattern.
+func match(fileName string, total int) (string, error) {
+	pieces := strings.Split(fileName, ".")
+	ext := pieces[len(pieces)-1]
+	tmp := strings.Join(pieces[0:len(pieces)-1], ".")
+
+	pieces = strings.Split(tmp, "_")
+	name := strings.Join(pieces[0:len(pieces)-1], "_")
+	number, err := strconv.Atoi(pieces[len(pieces)-1])
+	if err != nil {
+		return "", fmt.Errorf("%s didnt match our pattern", fileName)
+	}
+
+	return fmt.Sprintf("%s - %d of %d.%s", cases.Title(language.Und, cases.NoLower).String(name), number, total, ext), nil
+}
