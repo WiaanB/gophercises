256f565d81780a88ad321bbaa9dab335aec205b4
WiaanB <whoppingwaki@gmail.com>
2022-11-09 23:30:29 +0200

    nonrecursive renaming

--- file_renamer/main.go
+++ file_renamer/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
+	"path/filepath"
 	"strconv"
 	"strings"
 
@@ -11,19 +13,49 @@ import (
 )
 
 func main() {
-	fileName := "birthday_001.txt" // => Birthday - 1 of 4.txt
-	newName, err := match(fileName, 4)
+	// fileName := "birthday_001.txt" // => Birthday - 1 of 4.txt
+	// newName, err := match(fileName, 4)
+	// if err != nil {
+	// 	fmt.Println("no match")
+	// 	os.Exit(1)
+	// }
+	// fmt.Println(newName)
+	dir := "./sample"
+	files, err := ioutil.ReadDir(dir)
 	if err != nil {
-		fmt.Println("no match")
-		os.Exit(1)
+		panic(err)
 	}
-	fmt.Println(newName)
+	count := 0
+	var toRename []string
+	for _, file := range files {
+		if !file.IsDir() {
+			_, err := match(file.Name(), 4)
+			if err == nil {
+				count++
+				toRename = append(toRename, file.Name())
+			}
+		}
+	}
+
+	for _, origFileName := range toRename {
+		origPath := filepath.Join(dir, origFileName)
+		newName, err := match(origFileName, count)
+		if err != nil {
+			panic(err)
+		}
+		newPath := filepath.Join(dir, newName)
+		fmt.Printf("mv %s => %s\n", origPath, newPath)
+		err = os.Rename(origPath, newPath)
+		if err != nil {
+			panic(err)
+		}
+	}
 }
 
 // match returns the new file name, or an error if the file name
 // didn't match our pattern.
-func match(fileName string, total int) (string, error) {
-	pieces := strings.Split(fileName, ".")
+func match(filename string, total int) (string, error) {
+	pieces := strings.Split(filename, ".")
 	ext := pieces[len(pieces)-1]
 	tmp := strings.Join(pieces[0:len(pieces)-1], ".")
 
@@ -31,7 +63,7 @@ func match(fileName string, total int) (string, error) {
 	name := strings.Join(pieces[0:len(pieces)-1], "_")
 	number, err := strconv.Atoi(pieces[len(pieces)-1])
 	if err != nil {
-		return "", fmt.Errorf("%s didnt match our pattern", fileName)
+		return "", fmt.Errorf("%s didnt match our pattern", filename)
 	}
 
 	return fmt.Sprintf("%s - %d of %d.%s", cases.Title(language.Und, cases.NoLower).String(name), number, total, ext), nil
